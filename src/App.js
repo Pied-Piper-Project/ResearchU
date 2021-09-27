28a0d338a19dd114cf523b888585b86e2574b7df
EnocFlores <33188190+EnocFlores@users.noreply.github.com>
2021-09-27 02:20:11 -0400

    deploy react app to github pages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Look yall, I made our react app for ResearchU!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
