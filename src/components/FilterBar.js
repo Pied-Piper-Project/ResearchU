2d8da8fb195965154fd3a9f9a3389f67eeaffbdc
Abner B <Abner3@users.noreply.github.com>
2021-11-02 21:29:12 -0400

    What I have so far. Implemented Location, Semester, and DateRange. Still needs to debug.

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -3,29 +3,59 @@ import ReactSlider from "react-slider";
 
 const FilterBar = ({
   majors,
+  semesters,
   onAllFilter,
 }) => {
   const [filters, setFilters] = useState({
     year: "",
-    gpa: "",
+    gpa: 4,
     major: "",
+    isOnline: false,
+    semester: "",
+    duration: "",
   });
 
   const handleInput = (field) => (event) => {
     let tool;
-    if (field === "gpa"){
+    if (field === "gpa") {
       tool = event;
     }
-    else if (field === "year"){
+    else if (field === "year") {
       tool = event.target.value
-      if (tool === filters.year){
+      if (tool === filters.year) {
         tool = ""
       }
     }
-    else{
+    else if (field === "semester" || field === "major") {
       tool = event.target.value
     }
-    const value = tool; 
+
+    else if (field === "isOnline") {
+      tool = event.target.value
+      console.log(tool)
+      if ((tool === "true" && filters.isOnline === true) || (tool === "false" && filters.isOnline === false)) {
+        tool = ""
+      }
+      if (tool === "true") {
+        tool = true
+      }
+      else if (tool === "false") {
+        tool = false
+      }
+    }
+
+    else if (field === "date") {
+      tool = event
+      console.log(tool)
+      if (tool === "") {
+        tool = ""
+      }
+    }
+
+    else {
+      tool = event.target.value
+    }
+    const value = tool;
 
     setFilters({
       ...filters,
@@ -33,15 +63,21 @@ const FilterBar = ({
     });
 
     switch (field) {
+      case "year":
+        onAllFilter(value, filters.gpa, filters.major, filters.isOnline, filters.semester, filters.date);
+        break;
       case "gpa":
-        onAllFilter(filters.year, value, filters.major);
+        onAllFilter(filters.year, value, filters.major, filters.isOnline, filters.semester, filters.date);
         break;
       case "major":
-        onAllFilter(filters.year, filters.gpa, value);
+        onAllFilter(filters.year, filters.gpa, value, filters.isOnline, filters.semester, filters.date);
         break;
-      case "year":
-        onAllFilter(value, filters.gpa, filters.major);
-        break; 
+      case "isOnline":
+        onAllFilter(filters.year, filters.gpa, filters.major, value, filters.semester, filters.date);
+      case "semester":
+        onAllFilter(filters.year, filters.gpa, filters.major, filters.isOnline, value, filters.date);
+      case "date":
+        onAllFilter(filters.year, filters.gpa, filters.major, filters.isOnline, filters.semester, value);
       default:
         break;
     }
@@ -52,48 +88,48 @@ const FilterBar = ({
       <div className="">
         <h1>School year:</h1>
         <label htmlFor="year"></label>
-        <div className ="school-filter">
-        <div>
-        <input type="radio" className = "filter__boxes"  id="year" checked={filters.year === "Freshman"}
-        onClick={handleInput("year")}
-        value="Freshman"/> Freshman
-        </div>
-        <div>
-        <input
-        type="radio"
-        className = "filter__boxes"
-        id="year"
-        checked={filters.year === "Sophomore"}
-        onClick={handleInput("year")}
-        value="Sophomore"/> Sophomore
-        </div>
-        <div>
-        <input
-        type="radio"
-        className = "filter__boxes"
-        id="year"
-        checked={filters.year === "Junior"}
-        onClick={handleInput("year")}
-        value="Junior"/> Junior
-        </div>
-        <div>
-        <input
-        type="radio"
-        className = "filter__boxes"
-        id="year"
-        checked={filters.year === "Senior"}
-        onClick={handleInput("year")}
-        value="Senior"/> Senior
-        </div>
-        <div>
-        <input
-        type="radio"
-        className = "filter__boxes"
-        id="year"
-        checked={filters.year === "Graduate"}
-        onClick={handleInput("year")}
-        value="Graduate"/> Graduate
-        </div>
+        <div className="school-filter">
+          <div>
+            <input type="radio" className="filter__boxes" id="year" checked={filters.year === "Freshman"}
+              onClick={handleInput("year")}
+              value="Freshman" /> Freshman
+          </div>
+          <div>
+            <input
+              type="radio"
+              className="filter__boxes"
+              id="year"
+              checked={filters.year === "Sophomore"}
+              onClick={handleInput("year")}
+              value="Sophomore" /> Sophomore
+          </div>
+          <div>
+            <input
+              type="radio"
+              className="filter__boxes"
+              id="year"
+              checked={filters.year === "Junior"}
+              onClick={handleInput("year")}
+              value="Junior" /> Junior
+          </div>
+          <div>
+            <input
+              type="radio"
+              className="filter__boxes"
+              id="year"
+              checked={filters.year === "Senior"}
+              onClick={handleInput("year")}
+              value="Senior" /> Senior
+          </div>
+          <div>
+            <input
+              type="radio"
+              className="filter__boxes"
+              id="year"
+              checked={filters.year === "Graduate"}
+              onClick={handleInput("year")}
+              value="Graduate" /> Graduate
+          </div>
         </div>
       </div>
 
@@ -102,41 +138,108 @@ const FilterBar = ({
         <h1>Min GPA:</h1>
         <label htmlFor="gpa"></label>
         <div>
-            <ReactSlider 
-                // marks={1}
-                markClassName = "example-mark"
-                min={0}
-                max={4}
-                step={0.1}
-                className="horizontal-slider"
-                thumbClassName="example-thumb"
-                trackClassName="example-track"
-                id="gpa"
-                onChange={handleInput("gpa")}
-                renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
-            />
+          <ReactSlider
+            // marks={1}
+            markClassName="example-mark"
+            min={0}
+            max={4}
+            step={0.1}
+            className="horizontal-slider"
+            thumbClassName="example-thumb"
+            trackClassName="example-track"
+            id="gpa"
+            onChange={handleInput("gpa")}
+            defaultValue={4}
+            renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+          />
         </div>
       </div>
 
       <div className="">
         <h1>Subject:</h1>
         <label htmlFor="major"></label>
-        <div className = "select-wrapper" >
-        <div className = "select-dropdown-subject">
-        <select
-          className=" "
-          id="major"
-          onChange={handleInput("major")}
-        >
-          {/* <option value="select">Select</option> */}
-          <option className = "" value="">CS, Math, Biology...</option>
-          {majors.map((major) => (
-            <option className = "" value={major} key={major}>
-              {major}
-            </option>
-          ))}
-        </select>
+        <div className="select-wrapper" >
+          <div className="select-dropdown-subject">
+            <select
+              className=" "
+              id="major"
+              onChange={handleInput("major")}
+            >
+              {/* <option value="select">Select</option> */}
+              <option className="" value="">CS, Math, Biology...</option>
+              {majors.map((major) => (
+                <option className="" value={major} key={major}>
+                  {major}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
+      </div>
+
+      <div className="">
+        <h1>Location:</h1>
+        <label htmlFor="isOnline"></label>
+        <div className="school-filter">
+          <div>
+            <input type="radio"
+              className="filter__boxes"
+              id="isOnline"
+              checked={filters.isOnline === true}
+              onClick={handleInput("isOnline")}
+              value="true" /> Remote
+          </div>
+          <div>
+            <input
+              type="radio"
+              className="filter__boxes"
+              id="isOnline"
+              checked={filters.isOnline === false}
+              onClick={handleInput("isOnline")}
+              value="false" /> In-person
+          </div>
+        </div>
+      </div>
+
+      <div className="">
+        <h1>Semester:</h1>
+        <label htmlFor="semester"></label>
+        <div className="select-wrapper" >
+          <div className="select-dropdown-subject">
+            <select
+              className=" "
+              id="semester"
+              onChange={handleInput("semester")}>
+              {/* <option value="select">Select</option> */}
+              <option className="" value="">Select</option>
+              {semesters.map((semester) => (
+                <option className="" value={semester} key={semester}>
+                  {semester}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="">
+        <div className="select-wrapper">
+          <label htmlFor="startDate">From</label>
+          <input
+            type="date"
+            className="form-control"
+            id="startDate"
+            onChange={handleInput("from")}
+          />
+        </div>
+        <div className="select-wrapper">
+          <label htmlFor="endDate">To</label>
+          <input
+            type="date"
+            className="form-control"
+            id="endDate"
+            onChange={handleInput("to")}
+          />
         </div>
       </div>
     </div>
