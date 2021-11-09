d06d12af4a27581a5e794c9642c31f6395557c44
Schm2000 <ender.l.schmidt@emory.edu>
2021-11-09 14:04:51 -0500

    PIED-88 Create Post Fields
    Edited create post so that as many prerequisites and majors can be added if needed. This branch isn't complete at all, but I'm putting changes up now in the case that the backend needs to see the progress of the branch so far.

--- src/components/ManuallyCreatePost.js
+++ src/components/ManuallyCreatePost.js
@@ -7,48 +7,99 @@ function ManuallyCreatePost(){
         name: "",
         professor: "Stockington",
         school: "",
-        schoolLogo: "",
+        schoolLogo: "emory.edu",
         department: "",
-        isOnline: "",
-        location: "",
-        semester: "",
+        isOnline: false,
+        location: "N/A",
+        semester: "N/A",
         date: "2021-11-09T12:32:02Z",
         timeRange: "16:00-20:00",
-        payAmount: NaN,
-        gpa: NaN,
-        prerequisites: "",
-        postBody: "",
-        activePost: "",
-        isPaid: "",
-        isGrad: "",
-        year: "",
+        payAmount: 0,
+        postBody: "N/A",
+        activePost: false,
+        isPaid: false,
+        gpa: 0,
+        isGrad: false,
+        year: ["0"],
+        prerequisites: [""],
+        major: [""],
     });
+
+    const AddItemsToYear = (value)=>{
+        let list = post.year
+        list.push(value)
+        setPosts({
+            ...post,
+            ["year"]: list,
+        });
+    }
+
+    const AddPreq = () =>{
+        let list = post.prerequisites
+        list.push("")
+        setPosts({
+            ...post,
+            ["prerequisites"]: list,
+        });
+    }
+
+    const remPreq = index => {
+        let list = [...post.prerequisites]
+        list.splice(index, 1)
+        setPosts({
+            ...post,
+            ["prerequisites"]: list,
+        });
+    }
+
+    const addMajor = () => {
+        let list = post.major
+        list.push("")
+        setPosts({
+            ...post,
+            ["major"]: list,
+        });
+    }
+
+    const remMajor = index => {
+        let list = [...post.major]
+        list.splice(index, 1)
+        setPosts({
+            ...post,
+            ["major"]: list,
+        });
+    }
 
     const handleInput = (field) => (event) => {
         let {value} = event.target
-        if (field === "isOnline"){
-            if (value === post.isOnline){
-                value = ""
+        if (field === "isOnline" || field === "activePost" || field === "isPaid" || field === "isGrad"){
+            if (value === "true"){
+                value = true
             }
-        }
-        else if (field === "activePost"){
-            if (value === post.activePost){
-                value = ""
-            }
-        }
-        else if (field === "isPaid"){
-            if (value === post.isPaid){
-                value = ""
-            }
-        }
-        else if (field === "isGrad"){
-            if (value === post.isGrad){
-                value = ""
+            else{
+                value = false
             }
         }
         else if (field === "year"){
-            if (value === post.year){
-                value = ""
+            let subVal = value
+            if (post.year.includes("0")){
+                post.year = []
+            }
+            else if (post.year.includes(subVal)){
+                let index = post.year.indexOf(subVal)
+                let list = post.year
+                list.splice(index, 1)
+                if (post.year.length === 0){
+                    value = "0"
+                    post.year = []
+                }
+                else{
+                    setPosts({
+                        ...post,
+                        ["year"]: list,
+                    });
+                    return
+                }
             }
         }
         else if (field === "payAmount" || field === "gpa"){
@@ -56,22 +107,64 @@ function ManuallyCreatePost(){
             value = parseFloat(value, 10)
             if (isNaN(value) && p !== ""){
                 alert('Your value for ' + field + ' was not valid. Please insert a number.')
-                return
             }
         }
+        if (value === "" && field != "school" && field != "department" && field != "isOnline"
+            && field != "name" && field != "payAmount" && field != "gpa"){
+            value = "N/A"
+        }
+        else if ((field == "payAmount" || field == "gpa") && isNaN(value)){
+            value = 0
+        }
 
+        if (field === "year"){
+            AddItemsToYear(value)
+        }
+        else{
         setPosts({
             ...post,
             [field]: value,
         });
+        }
+    };
+
+    const handlePrereqInput = (e, index) => {
+        const {name, value} = e.target
+        let list = post.prerequisites
+        list[index] = value
+        setPosts({
+            ...post,
+            ["prerequisites"]: list,
+        })
+
+    };
+
+    const handleMajorInput = (e, index) => {
+        const {name, value} = e.target
+        let list = post.major
+        list[index] = value
+        setPosts({
+            ...post,
+            ["major"]: list,
+        })
+
     };
 
     const submitFormHandler = (event) => {
         event.preventDefault();
+        if (event.nativeEvent.submitter.tagName === "BUTTON"){
+            return
+        }
         for (var [field, value] of Object.entries(post)){
             if (value === ""){
-                alert('Error: ' + field + ' needs to be implemented')
-                return
+                if (field === "school" || field === "department" || field === "isOnline"
+                    || field === "name"){
+                    alert('Error: ' + field + ' needs to be implemented')
+                    return
+                }
+                else{
+                    value = "N/A"
+                }
             }
             else if ((field === "gpa" || field === "payAmount") && isNaN(value)){
                 alert('Error: ' + field + ' needs to be implemented')
@@ -104,9 +197,9 @@ function ManuallyCreatePost(){
                         <div className = "filter__container">
                             <div className="school-filter">
                                 <input type="radio" id="isOnline" className="filter__boxes" value = "true"
-                                    onClick = {handleInput("isOnline")} checked = {post.isOnline === "true"}/> Remote
+                                    onClick = {handleInput("isOnline")} checked = {post.isOnline === true}/> Remote
                                 <input type="radio" id="isOnline" className = "filter__boxes" value = "false"
-                                    onClick = {handleInput("isOnline")} checked = {post.isOnline === "false"}/> In-Person
+                                    onClick = {handleInput("isOnline")} checked = {post.isOnline === false}/> In-Person
                             </div>
                         </div>
                         <label className ="createPost-label"  for="location">Location:</label>
@@ -115,9 +208,9 @@ function ManuallyCreatePost(){
                         <div className = "filter__container">
                             <div className="school-filter">
                                 <input type="radio" id="activePost" className="filter__boxes" value = "true"
-                                    onClick = {handleInput("activePost")} checked = {post.activePost === "true"}/> Active
+                                    onClick = {handleInput("activePost")} checked = {post.activePost === true}/> Active
                                 <input type="radio" id="activePost" className = "filter__boxes" value = "false"
-                                    onClick = {handleInput("activePost")} checked = {post.activePost === "false"}/> Inactive
+                                    onClick = {handleInput("activePost")} checked = {post.activePost === false}/> Inactive
                             </div>
                         </div>
                         <label className ="createPost-label"  for="semester">Semester:</label>
@@ -126,9 +219,9 @@ function ManuallyCreatePost(){
                         <div className = "filter__container">
                             <div className="school-filter">
                                 <input type="radio" id="isPaid" className="filter__boxes" value = "true"
-                                    onClick = {handleInput("isPaid")} checked = {post.isPaid === "true"}/> Yes
+                                    onClick = {handleInput("isPaid")} checked = {post.isPaid === true}/> Yes
                                 <input type="radio" id="isPaid" className = "filter__boxes" value = "false"
-                                    onClick = {handleInput("isPaid")} checked = {post.isPaid === "false"}/> No
+                                    onClick = {handleInput("isPaid")} checked = {post.isPaid === false}/> No
                             </div>
                         </div>
                         <label className ="createPost-label"  for="payAmount">Pay Amount (number only):</label>
@@ -139,28 +232,50 @@ function ManuallyCreatePost(){
                         <div className = "filter__container">
                             <div className="school-filter">
                                 <input type="radio" id="isGrad" className="filter__boxes" value = "true"
-                                    onClick = {handleInput("isGrad")} checked = {post.isGrad === "true"}/> Yes
+                                    onClick = {handleInput("isGrad")} checked = {post.isGrad === true}/> Yes
                                 <input type="radio" id="isGrad" className = "filter__boxes" value = "false"
-                                    onClick = {handleInput("isGrad")} checked = {post.isGrad === "false"}/> No
+                                    onClick = {handleInput("isGrad")} checked = {post.isGrad === false}/> No
                             </div>
                         </div>
                         <label className ="createPost-label"  for="activePost">Year of College Preferred:</label>
                         <div className = "filter__container">
                             <div className="school-filter">
-                                <input type="radio" id="year" className="filter__boxes" value = "Freshman"
-                                    onClick = {handleInput("year")} checked = {post.year === "Freshman"}/> Freshman
-                                <input type="radio" id="year" className = "filter__boxes" value = "Sophomore"
-                                    onClick = {handleInput("year")} checked = {post.year === "Sophomore"}/> Sophomore
-                                <input type="radio" id="year" className = "filter__boxes" value = "Junior"
-                                    onClick = {handleInput("year")} checked = {post.year === "Junior"}/> Junior
-                                <input type="radio" id="year" className = "filter__boxes" value = "Senior"
-                                    onClick = {handleInput("year")} checked = {post.year === "Senior"}/> Senior
-                                <input type="radio" id="year" className = "filter__boxes" value = "Graduate"
-                                    onClick = {handleInput("year")} checked = {post.year === "Graduate"}/> Graduate
+                                <input type="radio" id="year" className="filter__boxes" value = "1"
+                                    onClick = {handleInput("year")} checked = {post.year.includes("1")}/> Freshman
+                                <input type="radio" id="year" className = "filter__boxes" value = "2"
+                                    onClick = {handleInput("year")} checked = {post.year.includes("2")}/> Sophomore
+                                <input type="radio" id="year" className = "filter__boxes" value = "3"
+                                    onClick = {handleInput("year")} checked = {post.year.includes("3")}/> Junior
+                                <input type="radio" id="year" className = "filter__boxes" value = "4"
+                                    onClick = {handleInput("year")} checked = {post.year.includes("4")}/> Senior
+                                <input type="radio" id="year" className = "filter__boxes" value = "5"
+                                    onClick = {handleInput("year")} checked = {post.year.includes("5")}/> Graduate
                             </div>
                         </div>
                         <label className ="createPost-label"  for="prerequisites">Prerequisites:</label>
-                        <input type="text" id="prerequisites" name="prerequisites" onChange = {handleInput("prerequisites")}/>
+                        {post.prerequisites.map((x, i) => {
+                            return(
+                                <div>
+                                <input type="text" id="prerequisites" name="prerequisites" value = {x} onChange = {e => handlePrereqInput(e, i)}/>
+                                <div className="btn-box">
+                                    {post.prerequisites.length !== 1 && <button
+                                    onClick={() => remPreq(i)}>Remove</button>}
+                                    {post.prerequisites.length - 1 === i && <button onClick={AddPreq}>Add</button>}
+                                </div>
+                                </div>
+                        );})}
+                        <label className ="createPost-label"  for="major">Majors:</label>
+                        {post.major.map((x, i) => {
+                            return(
+                                <div>
+                                <input type="text" id="major" name="major" value = {x} onChange = {e => handleMajorInput(e, i)}/>
+                                <div className="btn-box">
+                                    {post.major.length !== 1 && <button
+                                    onClick={() => remMajor(i)}>Remove</button>}
+                                    {post.major.length - 1 === i && <button onClick={addMajor}>Add</button>}
+                                </div>
+                                </div>
+                        );})}
                         <label className ="createPost-label"  for="postBody">Post Body (Write a description of the post here):</label>
                         <input type="text" id="postBody" name="postBody" onChange = {handleInput("postBody")}/>
                         <input type="submit"></input>
