f57ec4214c6976d89abf33fafbf36db92d789cf2
Schm2000 <ender.l.schmidt@emory.edu>
2021-11-23 11:41:52 -0500

    PIED-26
    I updated the Contact Page to include the survey. If there's anything to the survey that needs to be added or if you feel the UI of the Contact Page needs adjusted let me know.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,11 +8,17 @@ const Contact = () => {
                 <div className="send__container bd-container bd-grid glass">
                     <div className="send__content">
                         <h2 className="section-title-center send__title">Contact US</h2>
-                        <p className="send__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        <p className="send__description">If you have any questions or suggestions about the website, send us your email address so that we can get in contact with you!</p>
                         <form action="">
                             <div className="send__direction">
                                 <input type="text" placeholder="Your email address" className="send__input"/>
                                 <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="button sendButton">Send</a>
+                            </div>
+                        </form>
+                        <p className="send__description">Alternatively, you can fill out our feedback form!</p>
+                        <form action="">
+                            <div>
+                                <a href="https://docs.google.com/forms/d/e/1FAIpQLSc19J90IYJI4fhFWHKqwSE21e_gS546t3sGTcw1iGiUUD03Cg/viewform?usp=sf_link" className="button sendButtonTwo">Feedback</a>
                             </div>
                         </form>
                     </div>
